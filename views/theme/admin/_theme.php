8e16f65acc68605417b22d0e100bd46b7273bfd3
Caio <bladellano@gmail.com>
2022-02-19 14:56:11 -0300

    Ajustadando cadastro de veiculos

--- views/theme/admin/_theme.php
+++ views/theme/admin/_theme.php
@@ -225,7 +225,7 @@
           <img src="<?=SITE['root']?>/views/assets/admin/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"><?=$_SESSION['user']['first_name']?></a>
+          <a href="#" class="d-block"><?= isset($_SESSION['user']) ? $_SESSION['user']['first_name'] : "--"?></a>
         </div>
       </div>
 
